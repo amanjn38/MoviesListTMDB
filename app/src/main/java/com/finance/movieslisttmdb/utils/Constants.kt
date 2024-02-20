028a47dde27f32469080f0a1269780469fd3f85e
amanjn38 <amanjn38@gmail.com>
2024-02-20 17:26:06 +0530

    Removed keys

--- app/src/main/java/com/finance/movieslisttmdb/utils/Constants.kt
+++ app/src/main/java/com/finance/movieslisttmdb/utils/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
     companion object {
         const val BASE_URL_IMAGE = "https://image.tmdb.org/t/p/w600_and_h900_bestv2"
         const val BASE_URL = "https://api.themoviedb.org/3/"
-        const val API_KEY = "1dc0ed9e1fe10e2b04954001096c6f7e"
-        const val AUTHORIZATION_CODE = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZGMwZWQ5ZTFmZTEwZTJiMDQ5NTQwMDEwOTZjNmY3ZSIsInN1YiI6IjY1Y2U0NDY1ZDhhZjY3MDE2NDhmOTZiMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.boE326nsE3kXtrUOoXIi_QXlaNa3XFngRGBnRySYMuk"
+        const val API_KEY = ""
+        const val AUTHORIZATION_CODE = ""
     }
 }
